c3f836e71a539b949a4c387f6455f622d08cc969
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-31 13:49:18 -0400

    created store and actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
